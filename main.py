db7eaa038d3f319980d7bb8c9121ea68687c7f72
Wegner, Moritz <moritz.wegner@bundesanzeiger.de>
2022-03-14 13:31:02 +0100

    add k8s deployment file

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-from time import sleep
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
 
+hostName = "localhost"
+serverPort = 8080
 
-while True:
-    print("Hello world")
-    sleep(1)
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
